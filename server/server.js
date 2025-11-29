7a8fdf3a626f3d4b1a0d849e9cfefaa2a12965f4
Pushpendra081 <pushpendra1.mdl@gmail.com>
2025-11-29 23:29:29 +0530

    Made Basic server

--- server/server.js
+++ server/server.js
@@ -15,4 +15,4 @@ app.use(cors());
 app.use("/api/status", (req,res) => res.send("Server is Live"));
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, ()=> console.log("Server is running on PORT" + PORT));
+server.listen(PORT, ()=> console.log("Server is running on PORT" + PORT)) ;
