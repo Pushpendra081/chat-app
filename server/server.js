c4f510d5f1567638e36b38264ae4e937b4266fd4
Pushpendra081 <pushpendra1.mdl@gmail.com>
2025-12-13 17:52:46 +0530

    Message Contrller API Endpoints and Send Message Function and Routes setup

--- server/server.js
+++ server/server.js
@@ -4,11 +4,39 @@ import cors from "cors";
 import http from "http";
 import { connectDB } from "./library/db.js";
 import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRouters.js";
+import { server } from "socket.io";
 
 // Create express app and HTTP server
 
 const app = express();
 const server = http.createServer(app);
+
+// Initialize socket.io server
+export const io = new Server(server, {
+    cors: {origin: "*"}
+})
+
+// Store Online users
+export const userSocketMap = {}; //{ userId: socketId}
+
+//Socket.io connection handler
+io.on("connection", (socket)=>{
+
+    const userId = socket.handshake.query.userId;
+    console.log("User Connected", userId);
+
+    if(userId) userSocketMap[userId] = socket.id;
+
+    // Emit online users to all connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnected", ()=>{
+        console.log("User Disconnected", userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap))        
+    })
+})
 
 // middleware setup
 app.use(express.json({limit: "4mb"}));
@@ -17,6 +45,7 @@ app.use(cors());
 //Route setup
 app.use("/api/status", (req,res) => res.send("Server is Live"));
 app.use("/api/auth", userRouter)
+app.use("/api/messages", messageRouter)
 
 // connect to mongoDB
 await connectDB();
